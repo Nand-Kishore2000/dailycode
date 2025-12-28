b5a7e38668bf6cc9184510caed556365edd67ac0
Nand-Kishore2000 <nanduchhimpa414243@gmail.com>
2025-12-28 18:56:06 +0530

    Learn about the basics of typescript and how to compile it

--- index.ts
+++ index.ts
@@ -1,3 +1,59 @@
 
-let x: number = 25;
-console.log("x > ",x+1);    
+
+function delayFn1(fn: ()=> void){
+
+    setTimeout(()=> {
+        fn()
+    },1000);
+
+}
+
+delayFn1(function(){ 
+    console.log("Hello world")
+})
+
+function delayFn(fn: ()=> number){
+
+    setTimeout(()=> {
+        fn()
+    },1000);
+
+}
+
+delayFn(function(){ 
+    console.log("Hello world")
+    return 5;
+})
+
+
+
+
+// function greet(name:string | number){
+//     console.log(`Hello ${name}`)
+// }
+
+// greet("Nandkishore");
+// greet(5);
+
+
+// function sum(a: number,b:number): number{
+//     return a+b;
+// }
+
+// const answer = sum(4,5);
+// console.log("Sum:", answer)
+
+
+// function sumStr(a: string,b:number): string{
+//     return a+b;
+// }
+
+// const str = sumStr("4",5); // "1" + 2 => 12 
+// console.log("Sum:", str)
+
+
+
+// // number, string, any
+// let x:any = 1;
+// x = "dfsdfsdf"
+// x = true
