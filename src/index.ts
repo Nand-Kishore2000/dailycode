140f8cbceec9ffe165599c71c91f20a0192449ca
Nand-Kishore2000 <nanduchhimpa414243@gmail.com>
2026-01-14 22:48:55 +0530

    Learned typescript - interface and Records

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 
+interface User{
+    name:string;
+    age:number;
+}
+
+function sumAge(user1:User, user2:User){
+    return user1.age + user2.age;
+}
+
+const ram:User = {
+    name:"Ram",
+    age:21
+}
+
+const shyam : User = {
+    name: "shyam",
+    age:22,
+}
+console.log(sumAge(ram, shyam));
 
 
-
-
-
+ 
 
 
 
